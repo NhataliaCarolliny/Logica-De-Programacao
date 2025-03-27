a535282e627e0f7478c1be3c174ebde4ec2306d0
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-03-27 17:27:52 -0300

    Add files via upload

---
+++ Exercício_Aula9/Questao5_Aula9_SomaDeNumerosPares.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int somaNumeros=0;
+    int numero;
+
+    printf("Digite um número: ");
+    scanf("%d", &numero);
+
+    for(int i = 1; i <= numero; i++) {
+        if(i % 2 == 0) {
+            somaNumeros += i;
+        }
+    }
+    printf("Soma dos pares de 1 até %d: %d", numero, somaNumeros );
+}

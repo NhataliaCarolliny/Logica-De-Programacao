f1c0ee46143b4b92df23702557f9ea6c88dc668e
Nhatalia Carolliny <nhataliacarolliny@gmail.com>
2025-03-27 16:53:47 -0300

    Add files via upload

---
+++ Exercício_Aula9/Questao3_Aula9_TabuadaDeUmNumero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int numero;
+
+    printf("De qual número voce quer sabe a tabuada? ");
+    scanf("%d", &numero);
+
+    for(int i = 0; i <= 10; i++) {
+        int resultado;
+        resultado=numero*i;
+        printf("%d X %d = %d \n", numero, i, resultado);
+    }
+}
